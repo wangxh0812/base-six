50de57f85bfbc07e7067e0c8094ea531c654573a
wangxuhui <wangxh0812@163.com>
2017-02-09 16:14:12 +0800

    aa

--- six-parent/six-web/src/main/java/com/six/WebApplication.java
+++ six-parent/six-web/src/main/java/com/six/WebApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.larva.framework.config.WebSpringSecurityConfig;
 
